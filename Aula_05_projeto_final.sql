8e6c7860997bcce1fcba2a7d389e554cee5ce214
vgmariucci <vgmariucci@hotmail.com>
2024-12-30 09:14:55 -0300

    meu primeiro commit Aula 05 Bootcamp SQL

---
+++ Aula_05_projeto_final.sql
@@ -0,0 +1,42 @@
+-- 1. Total revenues for 1997
+-- SELECT 
+--     SUM(od.quantity * od.unit_price * (1.0 - od.discount)) AS total_revenues_1997
+-- FROM 
+--     orders o
+-- JOIN 
+--     order_details od ON o.order_id = od.order_id
+-- WHERE 
+--     EXTRACT(YEAR FROM o.order_date) = 1997;
+
+
+-- 2. Monthly growth and YTD values analysis
+-- WITH monthly_sales AS (
+--     SELECT 
+--         DATE_TRUNC('month', o.order_date) AS month,
+--         SUM(od.quantity * od.unit_price) AS monthly_total
+--     FROM 
+--         orders o
+--     JOIN 
+--         order_details od ON o.order_id = od.order_id
+--     GROUP BY 
+--         DATE_TRUNC('month', o.order_date)
+-- ),
+-- cumulative_sales AS (
+--     SELECT 
+--         month,
+--         monthly_total,
+--         SUM(monthly_total) OVER (ORDER BY month) AS ytd_total
+--     FROM 
+--         monthly_sales
+-- )
+-- SELECT 
+--     month,
+--     monthly_total,
+--     LAG(monthly_total) OVER (ORDER BY month) AS previous_month_total,
+--     (monthly_total - LAG(monthly_total) OVER (ORDER BY month)) / NULLIF(LAG(monthly_total) OVER (ORDER BY month), 0) AS monthly_growth,
+--     ytd_total
+-- FROM 
+--     cumulative_sales
+-- ORDER BY 
+--     month;
+
